3e71e8b62b9eab7dfc3152bdccffc32231d7af63
whitewise95 <81284265+whitewise95@users.noreply.github.com>
2023-03-22 22:22:14 +0900

    엔티티클래스 개발2

--- src/main/java/com/example/jpastudy/domain/Address.java
+++ src/main/java/com/example/jpastudy/domain/Address.java
@@ -10,4 +10,13 @@ public class Address {
 	private String city;
 	private String zipcode;
 	private String street;
+
+	protected Address() {
+	}
+
+	public Address(String city, String zipcode, String street) {
+		this.city = city;
+		this.zipcode = zipcode;
+		this.street = street;
+	}
 }
