477b81d89a9e67b67336b94dd63cd5f49fd0f1be
whitewise95 <81284265+whitewise95@users.noreply.github.com>
2023-04-01 12:05:52 +0900

    주문 리포지토리 개발

---
+++ src/main/java/com/example/jpastudy/repository/OrderRepository.java
@@ -0,0 +1,26 @@
+package com.example.jpastudy.repository;
+
+import com.example.jpastudy.domain.Order;
+import java.util.List;
+import javax.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+	private final EntityManager em;
+
+	public void save(Order order) {
+		em.persist(order);
+	}
+
+	public Order findById(Long id) {
+		return em.find(Order.class, id);
+	}
+
+	// public List<Order> findAll() {
+	//
+	// }
+}
