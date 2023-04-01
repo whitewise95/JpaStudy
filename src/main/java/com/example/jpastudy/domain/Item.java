ac3774eced125f1702757b427a248e3861018534
whitewise95 <81284265+whitewise95@users.noreply.github.com>
2023-04-01 11:02:11 +0900

    상품 엔티티 개발

--- src/main/java/com/example/jpastudy/domain/Item.java
+++ src/main/java/com/example/jpastudy/domain/Item.java
@@ -1,5 +1,6 @@
 package com.example.jpastudy.domain;
 
+import com.example.jpastudy.exception.NotEnoughStockException;
 import com.example.jpastudy.jpa.base.BaseDomainWithId;
 import java.util.ArrayList;
 import java.util.List;
@@ -41,4 +42,24 @@ public abstract class Item extends BaseDomainWithId {
 	 */
 	@ManyToMany(mappedBy = "items")
 	private List<Category> categoryList = new ArrayList<>();
+
+	//region 비지니스 로직
+
+	/**
+	 * stockQuantity 증가
+	 * */
+	public void addStock(int quantity) {
+		this.stockQuantity += quantity;
+	}
+
+	/**
+	 * stockQuantity 감소
+	 * */
+	public void removeStock(int quantity){
+		int restStock = this.stockQuantity - quantity;
+		if (restStock < 0) {
+			throw new NotEnoughStockException("need more stock");
+		}
+	}
+	//endregion
 }
