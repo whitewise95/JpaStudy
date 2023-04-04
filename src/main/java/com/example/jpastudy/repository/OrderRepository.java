e7c337581898566a7f5dbd5ae2658c9493b58130
whitewise95 <81284265+whitewise95@users.noreply.github.com>
2023-04-04 21:38:22 +0900

    주문 검색 기능 개발

--- src/main/java/com/example/jpastudy/repository/OrderRepository.java
+++ src/main/java/com/example/jpastudy/repository/OrderRepository.java
@@ -1,9 +1,20 @@
 package com.example.jpastudy.repository;
 
+import com.example.jpastudy.domain.Member;
 import com.example.jpastudy.domain.Order;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 @Repository
 @RequiredArgsConstructor
@@ -19,7 +30,27 @@ public class OrderRepository {
 		return em.find(Order.class, id);
 	}
 
-	// public List<Order> findAll() {
-	//
-	// }
+	public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+		Root<Order> o = cq.from(Order.class);
+		Join<Order, Member> m = o.join("member", JoinType.INNER); //회원과 조인
+		List<Predicate> criteria = new ArrayList<>();
+		//주문 상태 검색
+		if (orderSearch.getOrderStatus() != null) {
+			Predicate status = cb.equal(o.get("status"),
+										orderSearch.getOrderStatus());
+			criteria.add(status);
+		}
+		//회원 이름 검색
+		if (StringUtils.hasText(orderSearch.getMemberName())) {
+			Predicate name =
+				cb.like(m.<String>get("name"), "%" +
+					orderSearch.getMemberName() + "%");
+			criteria.add(name);
+		}
+		cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+		TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000); //최대1000건
+		return query.getResultList();
+	}
 }
